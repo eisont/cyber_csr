d84d9ef41dc728667265e9c4bd6e6b45c1378de2
치훈 <rlaclgns321@naver.com>
2025-12-29 19:50:15 +0900

    feat: 사용자 정보 응답 타입 정의 및 성별 타입 추가

---
+++ src/types/response/user.types.ts
@@ -0,0 +1,16 @@
+import { RootState } from '@/redux';
+import { Gender } from '@/types/response';
+
+type LoginDataState = RootState['loginData'];
+
+export type UserInfoResponse = {
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  gender: Gender;
+  image: string;
+  accessToken: string;
+  refreshToken: string;
+};
