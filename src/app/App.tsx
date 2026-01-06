9fef3a3f007dd8a853ed3777593f6e62daf27e59
치훈 <rlaclgns321@naver.com>
2026-01-06 14:48:43 +0900

    refactor: routes 폴더 구조 및 파일명 정리하고 TS로 전환

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { AuthAndUserInfo, ScrollToTop } from '@/app/provider';
-import AppRouter from '@/app/router';
+import AppRouter from '@/app/routes';
 import { store } from '@/app/store';
 
 const App = () => {
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
+import { RootState } from '@/app/store';
 import Layout from '@/pages/Layout';
 import { CategoryHighlight, CategoryPromoBanner, HeroBanner, SeasonalSaleBanner } from '@/pages/ui';
 import ProductGrid from '@/shared/ui/ProductGrid';
@@ -11,8 +12,8 @@ const SelectUser = lazy(() => import('@/pages/SelectUser'));
 const MyPage = lazy(() => import('@/pages/Mypage'));
 const ProductDetail = lazy(() => import('@/pages/Explore/Detail'));
 
-const AppRouter = () => {
-  const userInfo = useSelector((state) => state.userInfo);
+const AppRoutes = () => {
+  const userInfo = useSelector((state: RootState) => state.userInfo);
 
   return (
     <Layout>
@@ -37,11 +38,11 @@ const AppRouter = () => {
           <Route path="/recipes" element={<Explore />} />
           <Route path="/search" element={<SearchProducts />} />
           <Route path="/selectUser" element={<SelectUser />} />
-          <Route path={`/${userInfo.username}`} element={<MyPage />} />
+          <Route path={`/${userInfo?.username}`} element={<MyPage />} />
         </Routes>
       </Suspense>
     </Layout>
   );
 };
 
-export default AppRouter;
+export default AppRoutes;
