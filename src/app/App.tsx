cdda9ebbec39af81955d8a7ea000b51b2225e767
치훈 <rlaclgns321@naver.com>
2026-01-10 22:36:11 +0900

    chore: QueryClientProvider로 앱 전역 감싸기

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,22 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { AuthAndUserInfo, ScrollToTop } from '@/app/provider';
 import AppRouter from '@/app/routes';
 import { store } from '@/app/store';
+import { queryClient } from '@/shared/query/queryClient';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <ScrollToTop />
-      <Provider store={store}>
-        <AuthAndUserInfo />
-        <AppRouter />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <ScrollToTop />
+        <Provider store={store}>
+          <AuthAndUserInfo />
+          <AppRouter />
+        </Provider>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
